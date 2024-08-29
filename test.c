8553d7ef08d7c4c67541e6b05ba23bbd94d8a06f
María Marta Ramírez Gil <ram21342@uvg.edu.gt>
2024-08-29 14:59:54 -0600

    fix

--- test.c
+++ test.c
@@ -6,35 +6,15 @@
 #include <omp.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-#include <dirent.h>
-#include <unistd.h>
 
 #define WIDTH 640
 #define HEIGHT 480
 #define MAX_FRAMES 100 // Límite de creación de imágenes (puede ser modificado)
 
-// Función para eliminar archivos en la carpeta frames
-void clear_frames_directory() {
-    DIR *dir = opendir("frames");
-    struct dirent *entry;
-    char filepath[256];
-
-    if (dir != NULL) {
-        while ((entry = readdir(dir)) != NULL) {
-            // Ignorar . y ..
-            if (entry->d_name[0] != '.') {
-                snprintf(filepath, sizeof(filepath), "frames/%s", entry->d_name);
-                remove(filepath);
-            }
-        }
-        closedir(dir);
-    }
-}
-
 void draw_lissajous_curve(cairo_t *cr, int N, float time, int index) {
     int amplitude = 150;
     float a = 2.0f + index, b = 3.0f + index, delta = M_PI / 2;
-
+    
     cairo_set_line_width(cr, 1.0);
     cairo_set_source_rgb(cr, (rand() % 256) / 255.0, (rand() % 256) / 255.0, (rand() % 256) / 255.0);
     cairo_move_to(cr, WIDTH / 2, HEIGHT / 2);
@@ -45,7 +25,7 @@ void draw_lissajous_curve(cairo_t *cr, int N, float time, int index) {
         int y = HEIGHT / 2 + amplitude * sin(b * t + delta + time);
         cairo_line_to(cr, x, y);
     }
-
+    
     cairo_stroke(cr);
 }
 
@@ -61,7 +41,7 @@ void save_frame_as_image(int frame_num, int num_curves) {
 
     #pragma omp parallel for
     for (int i = 0; i < num_curves; i++) {
-        draw_lissajous_curve(cr, 1000, frame_num / 30.0, i);
+        draw_lissajous_curve(cr, 1000, frame_num / 30.0, i); // Ajuste de movimiento
     }
 
     cairo_surface_write_to_png(surface, filename);
@@ -71,7 +51,7 @@ void save_frame_as_image(int frame_num, int num_curves) {
 }
 
 void create_gif(int num_frames) {
-    system("convert -delay 10 -loop 0 frames/frame_*.png lissajous.gif");
+    system("convert -delay 10 -loop 0 frames/frame_*.png lissajous.gif"); // Ajuste de delay
 }
 
 int main(int argc, char *argv[]) {
@@ -82,18 +62,15 @@ int main(int argc, char *argv[]) {
 
     int N = atoi(argv[1]);
     int num_curves = atoi(argv[2]);
-
+    
     if (N > MAX_FRAMES) {
-        N = MAX_FRAMES;
+        N = MAX_FRAMES; // Límite de creación de imágenes
     }
 
     srand(time(NULL));
 
     // Crear directorio para los frames
     mkdir("frames", 0777);
-
-    // Limpiar la carpeta frames al inicio
-    clear_frames_directory();
 
     int frame_count = 0;
     while (frame_count < N) {
